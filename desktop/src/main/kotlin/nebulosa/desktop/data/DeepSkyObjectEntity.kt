01781b8b489dc005eca080903c1996aad6f5e303
tiagohm <tiago.henrique.cco@gmail.com>
2023-05-05 14:21:08 -0300

    Update

--- desktop/src/main/kotlin/nebulosa/desktop/data/DeepSkyObjectEntity.kt
+++ desktop/src/main/kotlin/nebulosa/desktop/data/DeepSkyObjectEntity.kt
@@ -159,6 +159,36 @@ class DeepSkyObjectEntity : SkyObject, HasAxisSize, Serializable {
 
         if (id != other.id) return false
         if (names != other.names) return false
+        if (m != other.m) return false
+        if (ngc != other.ngc) return false
+        if (ic != other.ic) return false
+        if (c != other.c) return false
+        if (b != other.b) return false
+        if (sh2 != other.sh2) return false
+        if (vdb != other.vdb) return false
+        if (rcw != other.rcw) return false
+        if (ldn != other.ldn) return false
+        if (lbn != other.lbn) return false
+        if (cr != other.cr) return false
+        if (mel != other.mel) return false
+        if (pgc != other.pgc) return false
+        if (ugc != other.ugc) return false
+        if (arp != other.arp) return false
+        if (vv != other.vv) return false
+        if (dwb != other.dwb) return false
+        if (tr != other.tr) return false
+        if (st != other.st) return false
+        if (ru != other.ru) return false
+        if (vdbha != other.vdbha) return false
+        if (ced != other.ced) return false
+        if (pk != other.pk) return false
+        if (png != other.png) return false
+        if (snrg != other.snrg) return false
+        if (aco != other.aco) return false
+        if (hcg != other.hcg) return false
+        if (eso != other.eso) return false
+        if (vdbh != other.vdbh) return false
+        if (mType != other.mType) return false
         if (magnitude != other.magnitude) return false
         if (rightAscension != other.rightAscension) return false
         if (declination != other.declination) return false
@@ -167,6 +197,9 @@ class DeepSkyObjectEntity : SkyObject, HasAxisSize, Serializable {
         if (parallax != other.parallax) return false
         if (radialVelocity != other.radialVelocity) return false
         if (distance != other.distance) return false
+        if (majorAxis != other.majorAxis) return false
+        if (minorAxis != other.minorAxis) return false
+        if (orientation != other.orientation) return false
         if (pmRA != other.pmRA) return false
         if (pmDEC != other.pmDEC) return false
         return constellation == other.constellation
@@ -175,6 +208,36 @@ class DeepSkyObjectEntity : SkyObject, HasAxisSize, Serializable {
     override fun hashCode(): Int {
         var result = id
         result = 31 * result + names.hashCode()
+        result = 31 * result + m
+        result = 31 * result + ngc
+        result = 31 * result + ic
+        result = 31 * result + c
+        result = 31 * result + b
+        result = 31 * result + sh2
+        result = 31 * result + vdb
+        result = 31 * result + rcw
+        result = 31 * result + ldn
+        result = 31 * result + lbn
+        result = 31 * result + cr
+        result = 31 * result + mel
+        result = 31 * result + pgc
+        result = 31 * result + ugc
+        result = 31 * result + arp
+        result = 31 * result + vv
+        result = 31 * result + dwb
+        result = 31 * result + tr
+        result = 31 * result + st
+        result = 31 * result + ru
+        result = 31 * result + vdbha
+        result = 31 * result + (ced?.hashCode() ?: 0)
+        result = 31 * result + (pk?.hashCode() ?: 0)
+        result = 31 * result + (png?.hashCode() ?: 0)
+        result = 31 * result + (snrg?.hashCode() ?: 0)
+        result = 31 * result + (aco?.hashCode() ?: 0)
+        result = 31 * result + (hcg?.hashCode() ?: 0)
+        result = 31 * result + (eso?.hashCode() ?: 0)
+        result = 31 * result + (vdbh?.hashCode() ?: 0)
+        result = 31 * result + (mType?.hashCode() ?: 0)
         result = 31 * result + magnitude.hashCode()
         result = 31 * result + rightAscension.hashCode()
         result = 31 * result + declination.hashCode()
@@ -183,6 +246,9 @@ class DeepSkyObjectEntity : SkyObject, HasAxisSize, Serializable {
         result = 31 * result + parallax.hashCode()
         result = 31 * result + radialVelocity.hashCode()
         result = 31 * result + distance.hashCode()
+        result = 31 * result + majorAxis.hashCode()
+        result = 31 * result + minorAxis.hashCode()
+        result = 31 * result + orientation.hashCode()
         result = 31 * result + pmRA.hashCode()
         result = 31 * result + pmDEC.hashCode()
         result = 31 * result + constellation.hashCode()
@@ -190,9 +256,14 @@ class DeepSkyObjectEntity : SkyObject, HasAxisSize, Serializable {
     }
 
     override fun toString(): String {
-        return "DeepSkyObjectEntity(id=$id, names=$names, magnitude=$magnitude, " +
-                "rightAscension=$rightAscension, declination=$declination, type=$type, " +
-                "redshift=$redshift, parallax=$parallax, radialVelocity=$radialVelocity, " +
-                "distance=$distance, pmRA=$pmRA, pmDEC=$pmDEC, constellation=$constellation)"
+        return "DeepSkyObjectEntity(id=$id, names='$names', m=$m, ngc=$ngc, ic=$ic, c=$c," +
+                " b=$b, sh2=$sh2, vdb=$vdb, rcw=$rcw, ldn=$ldn, lbn=$lbn, cr=$cr, mel=$mel," +
+                " pgc=$pgc, ugc=$ugc, arp=$arp, vv=$vv, dwb=$dwb, tr=$tr, st=$st, ru=$ru," +
+                " vdbha=$vdbha, ced=$ced, pk=$pk, png=$png, snrg=$snrg, aco=$aco, hcg=$hcg," +
+                " eso=$eso, vdbh=$vdbh, mType=$mType, magnitude=$magnitude," +
+                " rightAscension=$rightAscension, declination=$declination, type=$type," +
+                " redshift=$redshift, parallax=$parallax, radialVelocity=$radialVelocity," +
+                " distance=$distance, majorAxis=$majorAxis, minorAxis=$minorAxis," +
+                " orientation=$orientation, pmRA=$pmRA, pmDEC=$pmDEC, constellation=$constellation)"
     }
 }
