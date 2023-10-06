79fde34f610eab7c7a629326ef1db251964f7125
tiagohm <tiago.henrique.cco@gmail.com>
2023-10-05 22:54:08 -0300

    [api]: Update Star/DSO Catalog
    v2023.10.05

--- api/src/main/kotlin/nebulosa/api/image/ImageService.kt
+++ api/src/main/kotlin/nebulosa/api/image/ImageService.kt
@@ -18,6 +18,7 @@ import nebulosa.platesolving.astrometrynet.LocalAstrometryNetPlateSolver
 import nebulosa.platesolving.astrometrynet.NovaAstrometryNetPlateSolver
 import nebulosa.platesolving.watney.WatneyPlateSolver
 import nebulosa.sbd.SmallBodyDatabaseService
+import nebulosa.simbad.SimbadCatalogType
 import nebulosa.simbad.SimbadService
 import nebulosa.simbad.SimbadSkyCatalog
 import nebulosa.skycatalog.ClassificationType
@@ -196,6 +197,10 @@ class ImageService(
                 catalog.search(calibration.rightAscension, calibration.declination, calibration.radius, types)
 
                 for (entry in catalog) {
+                    if (SimbadCatalogType.entries.none { it.matches(entry.name) }) {
+                        continue
+                    }
+
                     val (x, y) = wcs.skyToPix(entry.rightAscensionJ2000, entry.declinationJ2000)
                     val annotation = if (entry.type.classification == ClassificationType.STAR) ImageAnnotation(x, y, star = entry)
                     else ImageAnnotation(x, y, dso = entry)
