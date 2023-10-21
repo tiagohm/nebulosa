c63bdb00c840a0f1c3ec38a688efd4684c05b728
tiagohm <tiago.henrique.cco@gmail.com>
2023-10-21 00:36:45 -0300

    [api]: Fix SVBony new properties

--- nebulosa-indi-client/src/main/kotlin/nebulosa/indi/client/device/camera/SVBonyCamera.kt
+++ nebulosa-indi-client/src/main/kotlin/nebulosa/indi/client/device/camera/SVBonyCamera.kt
@@ -4,20 +4,32 @@ import nebulosa.indi.client.device.DeviceProtocolHandler
 import nebulosa.indi.protocol.INDIProtocol
 import nebulosa.indi.protocol.NumberVector
 
-internal class AsiCamera(
+internal class SVBonyCamera(
     handler: DeviceProtocolHandler,
     name: String,
 ) : CameraDevice(handler, name) {
+
+    @Volatile private var legacyProperties = false
 
     override fun handleMessage(message: INDIProtocol) {
         when (message) {
             is NumberVector<*> -> {
                 when (message.name) {
+                    "CCD_GAIN" -> {
+                        legacyProperties = true
+                        processGain(message, message["GAIN"]!!)
+                    }
+                    "CCD_OFFSET" -> {
+                        legacyProperties = true
+                        processOffset(message, message["OFFSET"]!!)
+                    }
                     "CCD_CONTROLS" -> {
                         if ("Gain" in message) {
+                            legacyProperties = false
                             processGain(message, message["Gain"]!!)
                         }
                         if ("Offset" in message) {
+                            legacyProperties = false
                             processOffset(message, message["Offset"]!!)
                         }
                     }
@@ -30,10 +42,12 @@ internal class AsiCamera(
     }
 
     override fun gain(value: Int) {
-        sendNewNumber("CCD_CONTROLS", "Gain" to value.toDouble())
+        if (legacyProperties) sendNewNumber("CCD_GAIN", "GAIN" to value.toDouble())
+        else sendNewNumber("CCD_CONTROLS", "Gain" to value.toDouble())
     }
 
     override fun offset(value: Int) {
-        sendNewNumber("CCD_CONTROLS", "Offset" to value.toDouble())
+        if (legacyProperties) sendNewNumber("CCD_OFFSET", "OFFSET" to value.toDouble())
+        else sendNewNumber("CCD_CONTROLS", "Offset" to value.toDouble())
     }
 }
