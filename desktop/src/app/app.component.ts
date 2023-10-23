bb0a9f1a8d3dd2a30abcd6dfef1a8fe9f51193ac
tiagohm <tiago.henrique.cco@gmail.com>
2023-10-23 14:00:58 -0300

    [desktop]: Apply random background colors on titlebar

--- desktop/src/app/app.component.ts
+++ desktop/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { AfterViewInit, Component } from '@angular/core'
 import { Title } from '@angular/platform-browser'
+import { ActivatedRoute } from '@angular/router'
+import random from 'random'
 import { APP_CONFIG } from '../environments/environment'
 import { ElectronService } from '../shared/services/electron.service'
-import { ActivatedRoute } from '@angular/router'
 
 @Component({
     selector: 'app-root',
@@ -11,9 +12,15 @@ import { ActivatedRoute } from '@angular/router'
 })
 export class AppComponent implements AfterViewInit {
 
+    static readonly BACKGROUND_COLORS = [
+        '#880E4F', '#4A148C', '#311B92', '#1A237E',
+        '#0D47A1', '#01579B', '#006064', '#004D40',
+        '#1B5E20', '#33691E', '#B71C1C',
+    ]
+
     pinned = false
     maximizable = false
-    backgroundColor = '#1A237E'
+    backgroundColor = AppComponent.BACKGROUND_COLORS[random.int(0, AppComponent.BACKGROUND_COLORS.length - 1)]
 
     get title() {
         return this.windowTitle.getTitle()
