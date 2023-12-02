26de30dcdd1031bdc07e3ca6f22bfc608168910d
tiagohm <tiago.henrique.cco@gmail.com>
2023-12-02 15:54:27 -0300

    [desktop]: Improve device list menu
    * disable menu item if device is not connected;
    * show warning if no device is connected;

--- desktop/src/shared/components/devicemenu/devicemenu.component.ts
+++ desktop/src/shared/components/devicemenu/devicemenu.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core'
-import { MenuItem } from 'primeng/api'
+import { Component, Input, ViewChild } from '@angular/core'
+import { MenuItem, MessageService } from 'primeng/api'
 import { SEPARATOR_MENU_ITEM } from '../../constants'
 import { Device } from '../../types'
 import { DialogMenuComponent } from '../dialogmenu/dialogmenu.component'
@@ -15,15 +15,26 @@ export class DeviceMenuComponent {
     readonly model: MenuItem[] = []
 
     @Input()
-    readonly modelAtFirst = true
+    readonly modelAtFirst: boolean = true
+
+    @Input()
+    readonly disableIfNotConnected: boolean = true
 
     @ViewChild('menu')
     private readonly menu!: DialogMenuComponent
+
+    constructor(private message: MessageService) { }
 
     show<T extends Device>(devices: T[]) {
         const model: MenuItem[] = []
 
         return new Promise<T | undefined>((resolve) => {
+            if (devices.length > 0 && !devices.find(e => e.connected)) {
+                resolve(undefined)
+                this.message.add({ severity: 'warn', detail: 'Please connect your equipment first!' })
+                return
+            }
+
             const subscription = this.menu.visibleChange.subscribe((visible) => {
                 if (!visible) {
                     subscription.unsubscribe()
@@ -31,7 +42,7 @@ export class DeviceMenuComponent {
                 }
             })
 
-            if (this.modelAtFirst) {
+            if (this.model.length > 0 && this.modelAtFirst) {
                 model.push(...this.model)
                 model.push(SEPARATOR_MENU_ITEM)
             }
@@ -40,13 +51,14 @@ export class DeviceMenuComponent {
                 model.push({
                     icon: 'mdi mdi-connection',
                     label: device.name,
+                    disabled: this.disableIfNotConnected && !device.connected,
                     command: () => {
                         resolve(device)
                     },
                 })
             }
 
-            if (!this.modelAtFirst) {
+            if (this.model.length > 0 && !this.modelAtFirst) {
                 model.push(SEPARATOR_MENU_ITEM)
                 model.push(...this.model)
             }
