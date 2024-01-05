12b7d08dd7626cbc13a4479c0e2153160451352f
tiagohm <tiago.henrique.cco@gmail.com>
2024-01-05 13:39:38 -0300

    [api]: Fix satellite groups was ignored

--- api/src/main/kotlin/nebulosa/api/atlas/SatelliteEntity.kt
+++ api/src/main/kotlin/nebulosa/api/atlas/SatelliteEntity.kt
@@ -1,6 +1,5 @@
 package nebulosa.api.atlas
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
 import nebulosa.api.entities.BoxEntity
@@ -10,5 +9,5 @@ data class SatelliteEntity(
     @Id(assignable = true) override var id: Long = 0L,
     var name: String = "",
     var tle: String = "",
-    @JsonIgnore var groups: MutableList<String> = ArrayList(0),
+    var groups: MutableList<String> = ArrayList(0),
 ) : BoxEntity
