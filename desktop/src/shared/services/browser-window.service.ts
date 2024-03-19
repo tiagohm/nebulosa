b32af06e2be2dbfe30321eb4ccb2473a5187e6a8
tiagohm <tiago.henrique.cco@gmail.com>
2024-03-19 10:59:47 -0300

    [desktop]: Improve Focuser layout

--- desktop/src/shared/services/browser-window.service.ts
+++ desktop/src/shared/services/browser-window.service.ts
@@ -40,7 +40,7 @@ export class BrowserWindowService {
     }
 
     openFocuser(options: OpenWindowOptionsWithData<Focuser>) {
-        Object.assign(options, { icon: 'focus', width: 348, height: 202 })
+        Object.assign(options, { icon: 'focus', width: 252, height: 252 })
         this.openWindow({ ...options, id: `focuser.${options.data.name}`, path: 'focuser' })
     }
 
