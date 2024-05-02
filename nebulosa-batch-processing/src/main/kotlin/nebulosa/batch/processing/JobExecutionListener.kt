e82606a9074b995d16af1f8fcf140aecd05a1f75
tiagohm <tiago.henrique.cco@gmail.com>
2024-05-02 08:15:55 -0300

    [api]: Remove batch-processing module

--- nebulosa-batch-processing/src/main/kotlin/nebulosa/batch/processing/JobExecutionListener.kt
+++
@@ -1,8 +0,0 @@
-package nebulosa.batch.processing
-
-interface JobExecutionListener {
-
-    fun beforeJob(jobExecution: JobExecution) = Unit
-
-    fun afterJob(jobExecution: JobExecution) = Unit
-}
