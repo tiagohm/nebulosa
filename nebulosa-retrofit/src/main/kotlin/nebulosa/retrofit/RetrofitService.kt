9bc086f8b3be31e89e449a2c63149cd53736efe0
tiagohm <tiago.henrique.cco@gmail.com>
2024-05-18 13:32:48 -0300

    [api]: Fix possible bug when deserialize enum

--- nebulosa-retrofit/src/main/kotlin/nebulosa/retrofit/RetrofitService.kt
+++ nebulosa-retrofit/src/main/kotlin/nebulosa/retrofit/RetrofitService.kt
@@ -2,7 +2,9 @@ package nebulosa.retrofit
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import okhttp3.ConnectionPool
 import okhttp3.OkHttpClient
@@ -18,29 +20,29 @@ abstract class RetrofitService(
     mapper: ObjectMapper? = null,
 ) {
 
-    protected val mapper by lazy { mapper ?: DEFAULT_MAPPER.copy()!! }
+    protected val jsonMapper: ObjectMapper by lazy { mapper ?: DEFAULT_MAPPER.copy() }
 
     protected open val converterFactory = emptyList<Converter.Factory>()
 
     protected open val callAdaptorFactory: CallAdapter.Factory? = null
 
-    protected open fun handleOkHttpClientBuilder(builder: OkHttpClient.Builder) = Unit
+    protected open fun withOkHttpClientBuilder(builder: OkHttpClient.Builder) = Unit
 
-    protected open fun handleObjectMapper(mapper: ObjectMapper) = Unit
+    protected open fun withObjectMapper(mapper: ObjectMapper) = Unit
 
     protected open val retrofit by lazy {
         val builder = Retrofit.Builder()
         builder.baseUrl(url.trim().let { if (it.endsWith("/")) it else "$it/" })
         builder.addConverterFactory(RawAsStringConverterFactory)
         builder.addConverterFactory(RawAsByteArrayConverterFactory)
-        builder.addConverterFactory(EnumToStringConverterFactory(this.mapper))
+        builder.addConverterFactory(EnumToStringConverterFactory(jsonMapper))
         converterFactory.forEach { builder.addConverterFactory(it) }
-        handleObjectMapper(this.mapper)
-        builder.addConverterFactory(JacksonConverterFactory.create(this.mapper))
+        withObjectMapper(jsonMapper)
+        builder.addConverterFactory(JacksonConverterFactory.create(jsonMapper))
         callAdaptorFactory?.also(builder::addCallAdapterFactory)
 
         with((httpClient ?: HTTP_CLIENT).newBuilder()) {
-            handleOkHttpClientBuilder(this)
+            withOkHttpClientBuilder(this)
             builder.client(build())
         }
 
@@ -49,7 +51,7 @@ abstract class RetrofitService(
 
     companion object {
 
-        @JvmStatic private val CONNECTION_POOL = ConnectionPool(32, 30L, TimeUnit.MINUTES)
+        @JvmStatic private val CONNECTION_POOL = ConnectionPool(32, 5L, TimeUnit.MINUTES)
 
         @JvmStatic private val HTTP_CLIENT = OkHttpClient.Builder()
             .connectionPool(CONNECTION_POOL)
@@ -59,9 +61,11 @@ abstract class RetrofitService(
             .callTimeout(60L, TimeUnit.SECONDS)
             .build()
 
-        @JvmStatic private val DEFAULT_MAPPER = ObjectMapper()
-            .registerModules(JavaTimeModule())
-            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)!!
+        @JvmStatic private val DEFAULT_MAPPER = JsonMapper.builder()
+            .addModule(JavaTimeModule())
+            .serializationInclusion(JsonInclude.Include.NON_NULL)
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .build()!!
     }
 }
