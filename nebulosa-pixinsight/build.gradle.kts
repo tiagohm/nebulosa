098a3e8b5f3ab63268b8f07599aa6ec1d1debcff
tiagohm <tiago.henrique.cco@gmail.com>
2024-06-03 11:31:16 -0300

    [api]: Create pixinsight module

---
+++ nebulosa-pixinsight/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+    id("maven-publish")
+}
+
+dependencies {
+    api(project(":nebulosa-common"))
+    api(project(":nebulosa-math"))
+    api(project(":nebulosa-plate-solving"))
+    api(project(":nebulosa-star-detection"))
+    api(project(":nebulosa-livestacking"))
+    implementation(project(":nebulosa-log"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("pluginMaven") {
+            from(components["java"])
+        }
+    }
+}
