2e064ff603449baf192c8f95fc30e36d22662dbc
tiagohm <tiago.henrique.cco@gmail.com>
2024-06-30 11:09:35 -0300

    [api]: Fix unit tests

--- api/src/main/kotlin/nebulosa/api/atlas/Location.kt
+++ api/src/main/kotlin/nebulosa/api/atlas/Location.kt
@@ -1,17 +1,20 @@
 package nebulosa.api.atlas
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import nebulosa.api.beans.converters.angle.DegreesDeserializer
+import nebulosa.api.beans.converters.angle.DegreesSerializer
 import nebulosa.api.beans.converters.distance.MetersDeserializer
+import nebulosa.api.beans.converters.distance.MetersSerializer
 import nebulosa.math.Angle
 import nebulosa.math.Distance
 import nebulosa.nova.position.GeographicCoordinate
 import nebulosa.time.TimeZonedInSeconds
 
 data class Location(
-    @field:JsonDeserialize(using = DegreesDeserializer::class) override val latitude: Angle = 0.0,
-    @field:JsonDeserialize(using = DegreesDeserializer::class) override val longitude: Angle = 0.0,
-    @field:JsonDeserialize(using = MetersDeserializer::class) override val elevation: Distance = 0.0,
+    @field:JsonSerialize(using = DegreesSerializer::class) @field:JsonDeserialize(using = DegreesDeserializer::class) override val latitude: Angle = 0.0,
+    @field:JsonSerialize(using = DegreesSerializer::class) @field:JsonDeserialize(using = DegreesDeserializer::class) override val longitude: Angle = 0.0,
+    @field:JsonSerialize(using = MetersSerializer::class) @field:JsonDeserialize(using = MetersDeserializer::class) override val elevation: Distance = 0.0,
     val offsetInMinutes: Int = 0,
 ) : GeographicCoordinate, TimeZonedInSeconds {
 
