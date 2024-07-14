a04fa652242e860ace5d3c086907e8ad0dcc46b5
tiagohm <tiago.henrique.cco@gmail.com>
2024-07-14 13:33:37 -0300

    [api]: Prevent start PixInsight with slot 0

--- nebulosa-pixinsight/src/main/kotlin/nebulosa/pixinsight/script/PixInsightHelper.kt
+++ nebulosa-pixinsight/src/main/kotlin/nebulosa/pixinsight/script/PixInsightHelper.kt
@@ -1,12 +1,13 @@
 package nebulosa.pixinsight.script
 
 import java.nio.file.Path
+import kotlin.math.max
 
 fun startPixInsight(executablePath: Path, slot: Int): PixInsightScriptRunner {
     val runner = PixInsightScriptRunner(executablePath)
 
     if (!PixInsightIsRunning(slot).use { it.runSync(runner).success }) {
-        if (!PixInsightStartup(slot).use { it.runSync(runner).success }) {
+        if (!PixInsightStartup(max(1, slot)).use { it.runSync(runner).success }) {
             throw IllegalStateException("unable to start PixInsight")
         }
     }
