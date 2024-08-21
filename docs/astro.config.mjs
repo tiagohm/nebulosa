f4343ef811c4b955b451464822341e4f6d9c976a
tiagohm <tiago.henrique.cco@gmail.com>
2024-08-21 15:01:02 -0300

    [docs]: Deploy to GitHub Pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://tiagohm.github.io',
+    base: '/nebulosa',
     integrations: [
         starlight({
             title: 'Nebulosa',
