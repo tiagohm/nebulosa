e45be072707e03f63f22d4d32071fa1166f1325a
tiagohm <tiago.henrique.cco@gmail.com>
2024-10-23 12:55:58 -0300

    [api]: Improve SPK Type 21 test

--- nebulosa-nasa/src/test/kotlin/SpkTest.kt
+++ nebulosa-nasa/src/test/kotlin/SpkTest.kt
@@ -1,12 +1,15 @@
 import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
 import nebulosa.io.seekableSource
+import nebulosa.math.toKilometers
+import nebulosa.math.toKilometersPerSecond
 import nebulosa.nasa.daf.RemoteDaf
 import nebulosa.nasa.daf.SourceDaf
 import nebulosa.nasa.spk.Spk
 import nebulosa.test.AbstractTest
 import nebulosa.test.concat
 import nebulosa.test.dataDirectory
+import nebulosa.time.TDB
 import nebulosa.time.TimeYMDHMS
 import nebulosa.time.UTC
 import org.junit.jupiter.api.Test
@@ -42,12 +45,14 @@ class SpkTest : AbstractTest() {
     @Test
     fun type2165803Didymos() {
         val spk = Spk(SourceDaf(dataDirectory.concat("65803 Didymos.bsp").seekableSource().autoClose()))
-        val (p, v) = spk[10, 2065803]!!.compute(UTC(TimeYMDHMS(2022, 12, 8, 20, 7, 15.0)))
-        p[0] shouldBe (1.231026319338612E-01 plusOrMinus 1e-2)
-        p[1] shouldBe (1.022833989843715E+00 plusOrMinus 1e-2)
-        p[2] shouldBe (4.567595812943146E-01 plusOrMinus 1e-2)
-        v[0] shouldBe (-1.739740083644565E-02 plusOrMinus 1e-2)
-        v[1] shouldBe (5.410812824810350E-03 plusOrMinus 1e-2)
-        v[2] shouldBe (3.549254153190032E-03 plusOrMinus 1e-2)
+        val (p, v) = spk[10, 20065803]!!.compute(TDB(TimeYMDHMS(2022, 12, 8, 20, 7, 15.0)))
+
+        // NASA JPL Horizons. Use "x-y axes of reference frame (equatorial or equatorial-aligned, inertial)" as reference plane.
+        p[0].toKilometers shouldBe (1.977535053091079E+07 plusOrMinus 1e-7)
+        p[1].toKilometers shouldBe (1.529838534725696E+08 plusOrMinus 1e-7)
+        p[2].toKilometers shouldBe (6.828308290877973E+07 plusOrMinus 1e-7)
+        v[0].toKilometersPerSecond shouldBe (-3.012392001258445E+01 plusOrMinus 1e-13)
+        v[1].toKilometersPerSecond shouldBe (9.383280160332168E+00 plusOrMinus 1e-13)
+        v[2].toKilometersPerSecond shouldBe (6.151639916225326E+00 plusOrMinus 1e-13)
     }
 }
