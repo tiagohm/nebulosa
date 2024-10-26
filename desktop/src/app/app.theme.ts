b6ba153afb9d89a23585e83458b8bc6deb665e58
tiagohm <tiago.henrique.cco@gmail.com>
2024-10-26 16:39:18 -0300

    [desktop]: Migrate layout to tailwind

--- desktop/src/app/app.theme.ts
+++ desktop/src/app/app.theme.ts
@@ -3,6 +3,36 @@ import { definePreset } from 'primeng/themes'
 import { Aura } from 'primeng/themes/aura'
 
 export const AppPreset = definePreset(Aura, {
+	semantic: {
+		primary: {
+			50: '{zinc.50}',
+			100: '{zinc.100}',
+			200: '{zinc.200}',
+			300: '{zinc.300}',
+			400: '{zinc.400}',
+			500: '{zinc.500}',
+			600: '{zinc.600}',
+			700: '{zinc.700}',
+			800: '{zinc.800}',
+			900: '{zinc.900}',
+			950: '{zinc.950}',
+		},
+		colorScheme: {
+			dark: {
+				primary: {
+					color: '{zinc.50}',
+					hoverColor: '{zinc.100}',
+					activeColor: '{zinc.200}',
+				},
+				highlight: {
+					background: 'rgba(250, 250, 250, .16)',
+					focusBackground: 'rgba(250, 250, 250, .24)',
+					color: 'rgba(255,255,255,.87)',
+					focusColor: 'rgba(255,255,255,.87)',
+				},
+			},
+		},
+	},
 	components: {
 		select: {
 			root: {
--- desktop/src/shared/components/device-name/device-name.component.ts
+++ desktop/src/shared/components/device-name/device-name.component.ts
@@ -4,9 +4,9 @@ import type { Device } from '../../types/device.types'
 @Component({
 	selector: 'neb-device-name',
 	template: `
-		<div class="flex flex-column justify-content-center gap-0">
-			<span class=font-bold>{{ device.name }}</span>
-			<div class="flex flex-row align-items-center text-xs text-gray-400 gap-2">
+		<div class="flex flex-col justify-center gap-0">
+			<span class="font-bold">{{ device.name }}</span>
+			<div class="flex flex-row items-center gap-2 text-xs text-gray-400">
 				<span>DRIVER: {{ device.driverName }}</span>
 				<span>VERSION: {{ device.driverVersion }}</span>
 			</div>
