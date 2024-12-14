c69959f8be3ae29f9f16ddf807df2ed9524009df
tiagohm <tiago.henrique.cco@gmail.com>
2024-12-14 00:19:01 -0300

    [desktop]: Improve PreferenceService

--- desktop/src/shared/services/preference.service.ts
+++ desktop/src/shared/services/preference.service.ts
@@ -1,110 +1,113 @@
-import { inject, Injectable } from '@angular/core'
-import { AlignmentPreference, alignmentPreferenceWithDefault, DEFAULT_ALIGNMENT_PREFERENCE } from '../types/alignment.types'
-import { DEFAULT_SKY_ATLAS_PREFERENCE, SkyAtlasPreference, skyAtlasPreferenceWithDefault } from '../types/atlas.types'
-import { AutoFocusPreference, autoFocusPreferenceWithDefault, DEFAULT_AUTO_FOCUS_PREFERENCE } from '../types/autofocus.type'
-import { CalibrationPreference, calibrationPreferenceWithDefault, DEFAULT_CALIBRATION_PREFERENCE } from '../types/calibration.types'
-import { Camera, CameraPreference, cameraPreferenceWithDefault, DEFAULT_CAMERA_PREFERENCE } from '../types/camera.types'
-import { DEFAULT_FLAT_WIZARD_PREFERENCE, FlatWizardPreference, flatWizardPreferenceWithDefault } from '../types/flat-wizard.types'
-import { DEFAULT_FOCUSER_PREFERENCE, Focuser, FocuserPreference, focuserPreferenceWithDefault } from '../types/focuser.types'
-import { DEFAULT_FRAMING_PREFERENCE, FramingPreference, framingPreferenceWithDefault } from '../types/framing.types'
-import { DEFAULT_GUIDER_PREFERENCE, GuiderPreference, guiderPreferenceWithDefault } from '../types/guider.types'
-import { DEFAULT_HOME_PREFERENCE, HomePreference, homePreferenceWithDefault } from '../types/home.types'
-import { DEFAULT_IMAGE_PREFERENCE, ImagePreference, imagePreferenceWithDefault } from '../types/image.types'
-import { DEFAULT_MOUNT_PREFERENCE, Mount, MountPreference, mountPreferenceWithDefault } from '../types/mount.types'
-import { DEFAULT_ROTATOR_PREFERENCE, Rotator, RotatorPreference, rotatorPreferenceWithDefault } from '../types/rotator.types'
-import { DEFAULT_SEQUENCER_PREFERENCE, SequencerPreference, sequencerPreferenceWithDefault } from '../types/sequencer.types'
-import { DEFAULT_SETTINGS_PREFERENCE, SettingsPreference, settingsPreferenceWithDefault } from '../types/settings.types'
-import { DEFAULT_WHEEL_PREFERENCE, Wheel, WheelPreference, wheelPreferenceWithDefault } from '../types/wheel.types'
-import { Undefinable } from '../utils/types'
-import { LocalStorageService } from './local-storage.service'
+import { Injectable } from '@angular/core'
+import { AlignmentPreference, alignmentPreferenceWithDefault } from '../types/alignment.types'
+import { SkyAtlasPreference, skyAtlasPreferenceWithDefault } from '../types/atlas.types'
+import { AutoFocusPreference, autoFocusPreferenceWithDefault } from '../types/autofocus.type'
+import { CalibrationPreference, calibrationPreferenceWithDefault } from '../types/calibration.types'
+import { Camera, CameraPreference, cameraPreferenceWithDefault } from '../types/camera.types'
+import { FlatWizardPreference, flatWizardPreferenceWithDefault } from '../types/flat-wizard.types'
+import { Focuser, FocuserPreference, focuserPreferenceWithDefault } from '../types/focuser.types'
+import { FramingPreference, framingPreferenceWithDefault } from '../types/framing.types'
+import { GuiderPreference, guiderPreferenceWithDefault } from '../types/guider.types'
+import { HomePreference, homePreferenceWithDefault } from '../types/home.types'
+import { ImagePreference, imagePreferenceWithDefault } from '../types/image.types'
+import { Mount, MountPreference, mountPreferenceWithDefault } from '../types/mount.types'
+import { Rotator, RotatorPreference, rotatorPreferenceWithDefault } from '../types/rotator.types'
+import { SequencerPreference, sequencerPreferenceWithDefault } from '../types/sequencer.types'
+import { SettingsPreference, settingsPreferenceWithDefault } from '../types/settings.types'
+import { Wheel, WheelPreference, wheelPreferenceWithDefault } from '../types/wheel.types'
 
-export class PreferenceData<T> {
+export class PreferenceItem<T> {
 	constructor(
-		private readonly storage: LocalStorageService,
 		private readonly key: string,
-		private readonly defaultValue: T | (() => T),
-		private readonly withDefault?: (value: T) => T,
+		private readonly withDefault: (value?: T) => T,
 	) {}
 
 	has() {
-		return this.storage.has(this.key)
+		return localStorage.getItem(this.key) !== null
 	}
 
-	get(defaultValue?: T | (() => T)): T {
-		const value = this.storage.get<T>(this.key, defaultValue ?? this.defaultValue)
-		return this.withDefault?.(value) ?? value
+	get(withDefault?: () => T) {
+		const value = localStorage.getItem(this.key)
+
+		if (value === null || value === 'undefined') {
+			return withDefault?.() ?? this.withDefault()
+		} else {
+			return this.withDefault(JSON.parse(value))
+		}
 	}
 
-	set(value: Undefinable<T>) {
-		this.storage.set(this.key, value)
+	set(value: T | undefined | null) {
+		if (value === undefined || value === null) {
+			this.remove()
+		} else {
+			localStorage.setItem(this.key, JSON.stringify(value))
+		}
 	}
 
 	remove() {
-		this.storage.delete(this.key)
+		localStorage.removeItem(this.key)
 	}
 }
 
 @Injectable({ providedIn: 'root' })
 export class PreferenceService {
-	private readonly storage = inject(LocalStorageService)
-
-	readonly home: PreferenceData<HomePreference>
-	readonly image: PreferenceData<ImagePreference>
-	readonly skyAtlas: PreferenceData<SkyAtlasPreference>
-	readonly alignment: PreferenceData<AlignmentPreference>
-	readonly calibration: PreferenceData<CalibrationPreference>
-	readonly guider: PreferenceData<GuiderPreference>
-	readonly framing: PreferenceData<FramingPreference>
-	readonly settings: PreferenceData<SettingsPreference>
+	readonly home: PreferenceItem<HomePreference>
+	readonly image: PreferenceItem<ImagePreference>
+	readonly skyAtlas: PreferenceItem<SkyAtlasPreference>
+	readonly alignment: PreferenceItem<AlignmentPreference>
+	readonly calibration: PreferenceItem<CalibrationPreference>
+	readonly guider: PreferenceItem<GuiderPreference>
+	readonly framing: PreferenceItem<FramingPreference>
+	readonly settings: PreferenceItem<SettingsPreference>
 
 	constructor() {
-		this.home = this.create<HomePreference>('home', () => structuredClone(DEFAULT_HOME_PREFERENCE), homePreferenceWithDefault)
-		this.image = this.create<ImagePreference>('image', () => structuredClone(DEFAULT_IMAGE_PREFERENCE), imagePreferenceWithDefault)
-		this.skyAtlas = this.create<SkyAtlasPreference>('atlas', () => structuredClone(DEFAULT_SKY_ATLAS_PREFERENCE), skyAtlasPreferenceWithDefault)
-		this.alignment = this.create<AlignmentPreference>('alignment', () => structuredClone(DEFAULT_ALIGNMENT_PREFERENCE), alignmentPreferenceWithDefault)
-		this.calibration = this.create<CalibrationPreference>('calibration', () => structuredClone(DEFAULT_CALIBRATION_PREFERENCE), calibrationPreferenceWithDefault)
-		this.guider = this.create<GuiderPreference>('guider', () => structuredClone(DEFAULT_GUIDER_PREFERENCE), guiderPreferenceWithDefault)
-		this.framing = this.create<FramingPreference>('framing', () => structuredClone(DEFAULT_FRAMING_PREFERENCE), framingPreferenceWithDefault)
-		this.settings = this.create<SettingsPreference>('settings', () => structuredClone(DEFAULT_SETTINGS_PREFERENCE), settingsPreferenceWithDefault)
+		this.home = this.create<HomePreference>('home', homePreferenceWithDefault)
+		this.image = this.create<ImagePreference>('image', imagePreferenceWithDefault)
+		this.skyAtlas = this.create<SkyAtlasPreference>('atlas', skyAtlasPreferenceWithDefault)
+		this.alignment = this.create<AlignmentPreference>('alignment', alignmentPreferenceWithDefault)
+		this.calibration = this.create<CalibrationPreference>('calibration', calibrationPreferenceWithDefault)
+		this.guider = this.create<GuiderPreference>('guider', guiderPreferenceWithDefault)
+		this.framing = this.create<FramingPreference>('framing', framingPreferenceWithDefault)
+		this.settings = this.create<SettingsPreference>('settings', settingsPreferenceWithDefault)
 	}
 
-	create<T>(key: string, defaultValue: T | (() => T), withDefault?: (value: T) => T) {
-		return new PreferenceData<T>(this.storage, key, defaultValue, withDefault)
+	create<T>(key: string, withDefault: (value?: T) => T) {
+		return new PreferenceItem<T>(key, withDefault)
 	}
 
 	wheel(wheel: Wheel) {
-		return this.create<WheelPreference>(`wheel.${wheel.name}`, () => structuredClone(DEFAULT_WHEEL_PREFERENCE), wheelPreferenceWithDefault)
+		return this.create<WheelPreference>(`wheel.${wheel.name}`, wheelPreferenceWithDefault)
 	}
 
 	camera(camera: Camera) {
-		return this.create<CameraPreference>(`camera.${camera.name}`, () => structuredClone(DEFAULT_CAMERA_PREFERENCE), cameraPreferenceWithDefault)
+		return this.create<CameraPreference>(`camera.${camera.name}`, cameraPreferenceWithDefault)
 	}
 
 	mount(mount: Mount) {
-		return this.create<MountPreference>(`mount.${mount.name}`, () => structuredClone(DEFAULT_MOUNT_PREFERENCE), mountPreferenceWithDefault)
+		return this.create<MountPreference>(`mount.${mount.name}`, mountPreferenceWithDefault)
 	}
 
 	focusOffsets(wheel: Wheel, focuser: Focuser) {
-		return this.create<number[]>(`focusOffsets.${wheel.name}.${focuser.name}`, () => new Array<number>(wheel.count).fill(0))
+		return this.create<number[]>(`focusOffsets.${wheel.name}.${focuser.name}`, (value) => value ?? new Array<number>(wheel.count).fill(0))
 	}
 
 	focuser(focuser: Focuser) {
-		return this.create<FocuserPreference>(`focuser.${focuser.name}`, () => structuredClone(DEFAULT_FOCUSER_PREFERENCE), focuserPreferenceWithDefault)
+		return this.create<FocuserPreference>(`focuser.${focuser.name}`, focuserPreferenceWithDefault)
 	}
 
 	rotator(rotator: Rotator) {
-		return this.create<RotatorPreference>(`rotator.${rotator.name}`, () => structuredClone(DEFAULT_ROTATOR_PREFERENCE), rotatorPreferenceWithDefault)
+		return this.create<RotatorPreference>(`rotator.${rotator.name}`, rotatorPreferenceWithDefault)
 	}
 
 	flatWizard(camera: Camera) {
-		return this.create<FlatWizardPreference>(`flatWizard.${camera.name}`, () => structuredClone(DEFAULT_FLAT_WIZARD_PREFERENCE), flatWizardPreferenceWithDefault)
+		return this.create<FlatWizardPreference>(`flatWizard.${camera.name}`, flatWizardPreferenceWithDefault)
 	}
 
 	autoFocus(camera: Camera, focuser: Focuser) {
-		return this.create<AutoFocusPreference>(`autoFocus.${camera.name}.${focuser.name}`, () => structuredClone(DEFAULT_AUTO_FOCUS_PREFERENCE), autoFocusPreferenceWithDefault)
+		return this.create<AutoFocusPreference>(`autoFocus.${camera.name}.${focuser.name}`, autoFocusPreferenceWithDefault)
 	}
 
 	sequencer(camera: Camera) {
-		return this.create<SequencerPreference>(`sequencer.${camera.name}`, () => structuredClone(DEFAULT_SEQUENCER_PREFERENCE), sequencerPreferenceWithDefault)
+		return this.create<SequencerPreference>(`sequencer.${camera.name}`, sequencerPreferenceWithDefault)
 	}
 }
